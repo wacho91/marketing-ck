ab312f3aa6af25ba48ed93d9ac507343ef675e4f
wacho91 <criskol.71@gmail.com>
2024-10-17 11:25:35 -0500

    Header section services complete

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,21 @@
 
+import serviceImg from "../../assets/2.png";
 
 const Services = () => {
   return (
-    <div>
-        Services Component
+    <div className="container py-12">
+        {/*Header Section*/}
+        <div className="py-12 flex justify-between items-center">
+            <h1 className="text-4xl xl:text-5xl font-bold max-w-[550px]">
+                Classes to Spark Your{" "}
+                <span className="text-gray-400 underline">Creativity</span>
+            </h1>
+            <p className="hidden md:block">
+                Notified to Spark your <br></br>
+                <span className="text-gray-400 underline">Balance</span>{" "}
+            </p>
+        </div>
+        {/*Card Section*/}
     </div>
   )
 }
