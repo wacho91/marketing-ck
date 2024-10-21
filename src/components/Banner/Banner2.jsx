1f8530bf56402fcb9f0f04edd1419a0723dad624
wacho91 <criskol.71@gmail.com>
2024-10-21 11:33:31 -0500

    Banner2 Complete

--- src/components/Banner/Banner2.jsx
+++ src/components/Banner/Banner2.jsx
@@ -8,11 +8,19 @@ const Banner2 = () => {
             {/* Banner Image section */}
             <div className="flex justify-center items-center">
                 <img src={BannerImg} alt="" className="w-[90%] md:w-[550px] xl:w-[600px] md:!scale-110"/>
-
             </div>
             {/* Text Content section */}
-            <div>
-
+            <div className="space-y-8 flex flex-col justify-center items-center text-center md:text-left py-20 px-10 md:pl-10 md:py-0 md:px-0 md:items-start xl:max-w-[400px] mx-auto">
+                <p className="uppercase">
+                    Tech addicts
+                </p>
+                <h1 className="text-4xl xl:text-5xl font-semibold text-black/80">
+                    They really understood our <br />
+                    <span className="text-gray-400 underline">Needs</span>
+                </h1>
+                <button className="primary-btn hover:bg-black  text-black hover:text-white duration-300">
+                    More News
+                </button>
             </div>
         </div>
     </div>
