a3b7f66f15173034f1640bb679195a41c4d8a3c1
wacho91 <criskol.71@gmail.com>
2024-10-22 10:14:25 -0500

    Email Component Complete

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,9 +1,35 @@
 
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa"
+import FooterImg from "../../assets/5.png"
+
+const bgStyle = {
+    backgroundImage: `url(${FooterImg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    
+}
 
 const Email = () => {
   return (
-    <div>
-        Email Component
+    <div style={bgStyle} >
+        <div className="container text-center space-y-10 py-10">
+            <div className="space-y-4">
+                <p className="text-3xl md:text-4xl font-bold">Join Our Email</p>
+                <p>Get 20% off per order</p>
+            </div>
+            <div className="space-y-5">
+                <button className="primary-btn text-black">
+                    Subscribe
+                </button>
+                <div className="flex flex-row justify-center gap-3">
+                    <FaFacebook />
+                    <FaInstagram />
+                    <FaLinkedin />
+                    <FaTwitter />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
