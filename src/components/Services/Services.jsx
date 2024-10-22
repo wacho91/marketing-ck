0ffeea26869fa4e63d0c86fb28ea74eae5311892
wacho91 <criskol.71@gmail.com>
2024-10-22 11:28:25 -0500

    Services Animation

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,17 @@
 
 import serviceImg from "../../assets/2.png";
 
+import { motion } from "framer-motion";
+
 const Services = () => {
   return (
     <div className="container py-12">
         {/*Header Section*/}
-        <div className="py-12 flex justify-between items-center">
+        <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            className="py-12 flex justify-between items-center"
+        >
             <h1 className="text-4xl xl:text-5xl font-bold max-w-[550px]">
                 Classes to Spark Your{" "}
                 <span className="text-gray-400 underline">Creativity</span>
@@ -14,17 +20,26 @@ const Services = () => {
                 Notified to Spark your <br></br>
                 <span className="text-gray-400 underline">Balance</span>{" "}
             </p>
-        </div>
+        </motion.div>
         {/*Card Section*/}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+            >
                 <img 
                     src={serviceImg} 
                     alt="service-image"
                     className="w-[300px] mx-auto md:max-w-[500px]" 
                 />
-            </div>
-            <div className="max-w-[300px] mx-auto space-y-4">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }} 
+                className="max-w-[300px] mx-auto space-y-4"
+            >
                 <img src={serviceImg} alt="image" className="w-14" />
                 <p className="uppercase font-semibold text-xl">Learn By doing</p>
                 <p className="text-gray-500 pl-6 border-l-2">
@@ -33,8 +48,13 @@ const Services = () => {
                     asperiores magnam? Assumenda perferendis optio dicta natus nobis,
                     cupiditate eveniet libero ratione quam iusto!
                 </p>
-            </div>
-            <div className="max-w-[300px] mx-auto space-y-4">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: 0.6 }} 
+                className="max-w-[300px] mx-auto space-y-4"
+            >
                 <img src={serviceImg} alt="" className="w-14" />
                 <p className="uppercase font-semibold text-xl">Learn By doing</p>
                 <p className="text-gray-500 pl-6 border-l-2">
@@ -43,8 +63,13 @@ const Services = () => {
                 asperiores magnam? Assumenda perferendis optio dicta natus nobis,
                 cupiditate eveniet libero ratione quam iusto!
                 </p>
-            </div>
-            <div className="max-w-[300px] mx-auto space-y-4">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: 0.8 }} 
+                className="max-w-[300px] mx-auto space-y-4"
+            >
                 <img src={serviceImg} alt="" className="w-14" />
                 <p className="uppercase font-semibold text-xl">Learn By doing</p>
                 <p className="text-gray-500 pl-6 border-l-2">
@@ -53,7 +78,7 @@ const Services = () => {
                 asperiores magnam? Assumenda perferendis optio dicta natus nobis,
                 cupiditate eveniet libero ratione quam iusto!
                 </p>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
